34add6c672a1d81fc7e06c69e137e2d641e5b87f
Sumanth <reddysaisumanth@gmail.com>
2021-07-02 23:57:18 +0530

    Commit to main

---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import "./Header.css";
+const Header = () => {
+    return (
+        <span onClick={()=>window.scroll(0,0)} className="header">Watchlist</span>
+
+    )
+};
+
+export default Header
